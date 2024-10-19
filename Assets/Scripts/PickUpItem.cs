0c353c45d698b6d2035523f212b3ba189236491a
Nandy_X_Coder <nabendudas2001@gmail.com>
2024-10-19 21:05:04 +0530

    implemented juicecan spwan mechanism in vending machine

---
+++ Assets/Scripts/PickUpItem.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class PickUpItem : MonoBehaviour
+{
+    [SerializeField] protected PickUpItemSO itemSO;
+    
+    protected void OnPickUpByPlayer(){
+
+    }
+    protected void AddToInventory(){
+
+    }
+    protected void RemoveFromWorld(){
+
+    }
+    protected void OnUse(){
+
+    }
+    protected void OnDrop(){
+
+    }
+}
--- Assets/Scripts/VendingMachine.cs
+++ Assets/Scripts/VendingMachine.cs
@@ -1,11 +1,11 @@
-using System;
 using System.Collections;
 using UnityEngine;
 
 public class VendingMachine : Interactable
 {
     [SerializeField] private Animator vendFlap;
-
+    [SerializeField] private PickUpItemSO spwanItemSO;
+    [SerializeField] private Transform spawnPoint;
     private static readonly int isVending = Animator.StringToHash("IsVending");
     
     private void Awake(){
@@ -18,9 +18,10 @@ public class VendingMachine : Interactable
         vendFlap.SetTrigger(isVending);
         Debug.Log(vendFlap.name);
         if(afterInteract){
-            new WaitForSeconds(2f);
+            //new WaitForSeconds(2f);
             StartCoroutine(ResetVendFlapTriggerAfterDelay(2f));
             Debug.Log("Vending");
+            StartCoroutine(SpwanWithDelay(1f));
         }
         // StartCoroutine(WaitForStartVending(2f));
         // vendFlap.ResetTrigger(isVending);
@@ -32,5 +33,11 @@ public class VendingMachine : Interactable
     yield return new WaitForSeconds(delay);
     vendFlap.ResetTrigger(isVending);
     Debug.Log("Trigger reset after delay");
+    }
+    IEnumerator SpwanWithDelay(float delay){
+        yield return new WaitForSeconds(delay);
+        PickUpItem item = Instantiate(spwanItemSO.itemPrefab, spawnPoint.position, spawnPoint.rotation * Quaternion.Euler(0, 0, 90)).GetComponent<PickUpItem>();
+        SpwanFromVendMachine spwanedItem = item.GetComponent<SpwanFromVendMachine>();
+        spwanedItem.OnSpwanAction();
     }
 }
